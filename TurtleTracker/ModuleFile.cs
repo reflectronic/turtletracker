fbd892ccd20c4e368c605c5f4eb6cd9ff96d5396
John Tur <john-tur@outlook.com>
2024-09-02 20:04:45 -0400

    Initial module file parser

---
+++ TurtleTracker/ModuleFile.cs
@@ -0,0 +1,192 @@
+using System.Buffers;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+namespace TurtleTracker;
+
+public sealed class ModuleFile
+{
+    private ModuleFile()
+    {
+    }
+
+    public static ModuleFile Parse(byte[] modFile)
+    {
+        static string FromNullPadded(ReadOnlySpan<byte> ascii)
+        {
+            return Encoding.ASCII.GetString(ascii.TrimEnd((byte)0));
+        }
+
+        var reader = new SequenceReader<byte>(new(modFile));
+
+        string name = FromNullPadded(reader.Read(20));
+
+        // 15 or 31 samples?
+        var tag = modFile.AsSpan(1080, 4);
+
+        if (!tag.SequenceEqual("M.K."u8))
+        {
+            throw new InvalidDataException("Unsupported module format.");
+        }
+
+        var samples = ImmutableArray.CreateBuilder<ModuleSample>();
+
+        for (var i = 0; i < 31; i++)
+        {
+            string sampleName = FromNullPadded(reader.Read(22));
+            var sampleLength = reader.ReadInt16BigEndian();
+            var finetune = reader.ReadByte();
+            var volume = reader.ReadByte();
+            var loopOffsetStart = reader.ReadInt16BigEndian();
+            var loopOffsetLength = reader.ReadInt16BigEndian();
+
+            samples.Add(new(
+                sampleName,
+                sampleLength,
+                finetune,
+                volume,
+                loopOffsetStart,
+                loopOffsetLength
+            ));
+        }
+
+        var positionCount = reader.ReadByte();
+        reader.Advance(1);
+        
+        var positions = reader.Read(128);
+
+        int patternCount = -1;
+        foreach (var p in positions)
+        {
+            if (patternCount < p)
+            {
+                patternCount = p;
+            }
+        }
+
+        reader.Advance(4);
+
+        var patterns = ImmutableArray.CreateBuilder<ModulePattern>();
+
+        for (int i = 0; i < patternCount; i++)
+        {
+            var divisions = ImmutableArray.CreateBuilder<ModuleDivision>(64);
+
+            for (var j = 0; j < 64; j++)
+            {
+                static ModuleNote ReadNote(ref SequenceReader<byte> reader)
+                {
+                    var b1 = reader.ReadByte();
+                    var b2 = reader.ReadByte();
+                    var b3 = reader.ReadByte();
+                    var b4 = reader.ReadByte();
+
+                    var sampleNumber = (byte)((b1 & 0xF0) | (b3 >> 4));
+                    var samplePeriod = (short)(b2 & ((b1 & 0xF) << 8));
+                    var effectCommand = (short)(b4 & ((b3 & 0xF) << 8));
+
+                    return new(sampleNumber, samplePeriod, effectCommand);
+                }
+
+                divisions.Add(new ModuleDivision(
+                    ReadNote(ref reader),
+                    ReadNote(ref reader),
+                    ReadNote(ref reader),
+                    ReadNote(ref reader)
+                ));
+            }
+
+            patterns.Add(new ModulePattern(divisions.MoveToImmutable()));
+        }
+
+        var sampleDatas = ImmutableArray.CreateBuilder<byte[]>();
+
+        foreach (var sample in samples) 
+        {
+            sampleDatas.Add(reader.Read(sample.Length).ToArray());
+        }
+
+        return new ModuleFile()
+        {
+            Name = name,
+            Samples = samples.DrainToImmutable(),
+            SampleData = sampleDatas.DrainToImmutable(),
+            Patterns = patterns.DrainToImmutable(),
+            Positions = positions[..positionCount].ToImmutableArray()
+        };
+    }
+
+    public required string Name { get; init; }
+
+    public ImmutableArray<ModuleSample> Samples { get; init; }
+
+    public ImmutableArray<byte[]> SampleData { get; init; }
+
+    public ImmutableArray<ModulePattern> Patterns { get; init; }
+
+    public ImmutableArray<byte> Positions { get; init; }
+}
+
+public static class SequenceReaderExtensions
+{
+    public static ReadOnlySpan<T> Read<T>(ref this SequenceReader<T> reader, long count)
+        where T : unmanaged, IEquatable<T>
+    {
+        var pos = reader.Position;
+        reader.Advance(count);
+
+        var seq = reader.Sequence.Slice(pos, reader.Position);
+        return seq.IsSingleSegment ? seq.FirstSpan : seq.ToArray();
+    }
+
+    public static byte ReadByte(ref this SequenceReader<byte> reader)
+    {
+        if (!reader.TryRead(out var res))
+        {
+            ThrowInvalidDataException();
+            return default;
+        }
+
+        return res;
+    }
+
+    public static short ReadInt16BigEndian(ref this SequenceReader<byte> reader)
+    {
+        if (!reader.TryReadBigEndian(out short res))
+        {
+            ThrowInvalidDataException();
+            return default;
+        }
+
+        return res;
+    }
+
+
+    static void ThrowInvalidDataException()
+    {
+        throw new InvalidDataException("Invalid module file.");
+    }
+
+}
+
+public sealed record ModuleSample(
+    string Name,
+    short Length,
+    byte Finetune,
+    byte Volume,
+    short LoopOffsetStart,
+    short LoopOffsetEnd);
+
+public readonly record struct ModuleNote(byte Sample, short SamplePeriod, short EffectCommand);
+
+public sealed record ModuleDivision(
+    ModuleNote Channel1,
+    ModuleNote Channel2,
+    ModuleNote Channel3,
+    ModuleNote Channel4);
+
+public sealed record ModulePattern(
+    ImmutableArray<ModuleDivision> Divisions
+);
---
+++ TurtleTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+using TurtleTracker;
+
+Console.WriteLine("Enter a file: ");
+var path = Console.ReadLine();
+
+var modData = File.ReadAllBytes(path!);
+
+var modFile = ModuleFile.Parse(modData);
+
+Console.WriteLine($"Name: {modFile.Name}");
